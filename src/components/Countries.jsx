3ed7e71231d31bccc129aa716331961cadb82e56
Himanshu Mehra <himanshumehra@Himanshus-MacBook-Air.local>
2024-11-23 20:59:33 +0530

    initial comment

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,4 +1,3 @@
-// Countries.jsx
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -36,25 +35,35 @@ const Countries = ({ selectedRegion, searchQuery, darkMode }) => {
         const { name, cca3, flags, population, region, capital } = country;
 
         return (
-          <article
-            key={cca3}
-            className={`shadow-md rounded-lg p-4 transition-transform transform hover:scale-105 hover:shadow-lg ${darkMode ? "bg-gray-800 text-white" : "bg-white text-black"}`}
+         
+          <Link
+            to={`/countries/${name.common}`} 
+            key={cca3} 
+            className="shadow-md rounded-lg transition-transform transform hover:scale-105 hover:shadow-lg"
+            state={{ name, cca3, flags, population, region, capital }} 
           >
-            <img src={flags.png} alt={name.common} className="w-full h-40 object-cover rounded-t-lg" />
-            <div className="mt-4">
-              <h3 className="text-lg font-bold">{name.common}</h3>
-              <h4 className="text-sm mt-2">
-                <strong>Population:</strong> <span>{population}</span>
-              </h4>
-              <h4 className="text-sm">
-                <strong>Region:</strong> <span>{region}</span>
-              </h4>
-              <h4 className="text-sm">
-                <strong>Capital:</strong> <span>{capital || "N/A"}</span>
-              </h4>
-              <Link to={`/countries/${name.common}`}>Learn More</Link>
-            </div>
-          </article>
+            <article
+              className={`p-4 ${darkMode ? "bg-gray-800 text-white" : "bg-white text-black"}`}
+            >
+              <img
+                src={flags.png}
+                alt={name.common}
+                className="w-full h-40 object-cover rounded-t-lg"
+              />
+              <div className="mt-4">
+                <h3 className="text-lg font-bold">{name.common}</h3>
+                <h4 className="text-sm mt-2">
+                  <strong>Population:</strong> <span>{population}</span>
+                </h4>
+                <h4 className="text-sm">
+                  <strong>Region:</strong> <span>{region}</span>
+                </h4>
+                <h4 className="text-sm">
+                  <strong>Capital:</strong> <span>{capital || "N/A"}</span>
+                </h4>
+              </div>
+            </article>
+          </Link>
         );
       })}
     </section>
